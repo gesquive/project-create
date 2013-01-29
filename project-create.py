0a8a296777a6e5b09696615555a4fe10cf464a9c
Gus Esquivel <gesquive@gmail.com>
2013-01-28 20:07:56 -0600

    Added internet update feature and info file

--- project-create.py
+++ project-create.py
@@ -16,10 +16,12 @@ __author__ = "Gus E"
 __copyright__ = "Copyright 2013"
 __credits__ = ["Gus E"]
 __license__ = "GPL"
-__version__ = "0.5"
+__version__ = "1.0.2"
 __maintainer__ = "Gus E"
 __email__ = ""
 __status__ = "Production"
+
+info_file_path = 'https://raw.github.com/gesquive/quick-create-project/master/create-project.info'
 
 #--------------------------------------
 # Configurable Constants
@@ -48,6 +50,9 @@ Options and arguments:
                                         (default: './project_name')
   -s --desc <description>           The description of the project.
   -o --overwrite                    Force an overwrite of an existing project.
+  -u --update                       Checks server for an update, replaces
+                                        the current version if there is a
+                                        newer version available.
   -h --help                         Prints this message.
   -v --verbose                      Writes all messages to console.
 
@@ -67,8 +72,8 @@ def main():
     global verbose, debug
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hvl:d:s:o", \
-        ["help", "verbose", "lang=", "dir=", "desc=", "overwrite"])
+        opts, args = getopt.getopt(sys.argv[1:], "hvul:d:s:o", \
+        ["help", "verbose", "update", "lang=", "dir=", "desc=", "overwrite"])
     except getopt.GetoptError, err:
         print str(err)
         sys.exit(2)
@@ -91,6 +96,9 @@ def main():
             debug = True
         elif o in ("-v", "--verbose"):
             verbose = True
+        elif o in ("-u", "--update"):
+            update(info_file_path)
+            sys.exit(0)
         elif o in ("-l", "--lang"):
             a = a.lower()
             if a in ("python", "py"):
@@ -253,6 +261,124 @@ def generate_cplusplus(project_name, project_path, project_description):
     make_file.write(makefile)
     make_file.flush()
     make_file.close()
+
+
+def update(info_file_path, force_update=False):
+    """
+    Attempts to download the update url in order to find if an update is needed.
+    If an update is needed, the current script is backed up and the update is
+    saved in its place.
+    """
+    import urllib
+    import re
+    from subprocess import call
+    def compare_versions(vA, vB):
+        """
+        Compares two version number strings
+        @param vA: first version string to compare
+        @param vB: second version string to compare
+        @author <a href="http_stream://sebthom.de/136-comparing-version-numbers-in-jython-pytho/">Sebastian Thomschke</a>
+        @return negative if vA < vB, zero if vA == vB, positive if vA > vB.
+        """
+        if vA == vB: return 0
+
+        def num(s):
+            if s.isdigit(): return int(s)
+            return s
+
+        seqA = map(num, re.findall('\d+|\w+', vA.replace('-SNAPSHOT', '')))
+        seqB = map(num, re.findall('\d+|\w+', vB.replace('-SNAPSHOT', '')))
+
+        # this is to ensure that 1.0 == 1.0.0 in cmp(..)
+        lenA, lenB = len(seqA), len(seqB)
+        for i in range(lenA, lenB): seqA += (0,)
+        for i in range(lenB, lenA): seqB += (0,)
+
+        rc = cmp(seqA, seqB)
+
+        if rc == 0:
+            if vA.endswith('-SNAPSHOT'): return -1
+            if vB.endswith('-SNAPSHOT'): return 1
+        return rc
+
+    # dl the info file and parse it for version and dl path
+    try:
+        http_stream = urllib.urlopen(info_file_path)
+        update_file = http_stream.read()
+        http_stream.close()
+    except IOError, (errno, strerror):
+        print "Unable to retrieve version data"
+        print "Error %s: %s" % (errno, strerror)
+        return
+
+    match_regex = re.search(r'version: (\S+)', update_file)
+    if not match_regex:
+        print "No version info could be found"
+        return
+    update_version  =  match_regex.group(1)
+    match_regex = re.search(r'from: (\S+)', update_file)
+    if not match_regex:
+        print "No update location was specified"
+        return
+    dl_url = match_regex.group(1)
+
+    if not update_version:
+        print "Unable to parse version data"
+        return
+
+    if force_update:
+        print "Forcing update, downloading version %s..." \
+            % update_version
+    else:
+        cmp_result = compare_versions(__version__, update_version)
+        if cmp_result < 0:
+            print "Newer version %s available, downloading..." % __version__
+        elif cmp_result > 0:
+            print "Local version %s newer then available %s, not updating." \
+                % (__version__, update_version)
+            return
+        else:
+            print "You already have the latest version."
+            return
+
+    # dl, backup, and save the updated script
+    app_path = os.path.realpath(sys.argv[0])
+
+    if not os.access(app_path, os.W_OK):
+        print "Cannot update --  unable to write to %s" % app_path
+
+    dl_path = app_path + ".new"
+    backup_path = app_path + ".old"
+    try:
+        dl_file = open(dl_path, 'w')
+        http_stream = urllib.urlopen(dl_url)
+        dl_file.write(http_stream.read())
+        http_stream.close()
+        dl_file.close()
+    except IOError, (errno, strerror):
+        print "Download failed"
+        print "Error %s: %s" % (errno, strerror)
+        return
+
+    try:
+        os.rename(app_path, backup_path)
+    except OSError, (errno, strerror):
+        print "Unable to rename %s to %s: (%d) %s" \
+            % (app_path, backup_path, errno, strerror)
+        return
+
+    try:
+        os.rename(dl_path, app_path)
+    except OSError, (errno, strerror):
+        print "Unable to rename %s to %s: (%d) %s" \
+            % (dl_path, app_path, errno, strerror)
+        return
+
+    os.chmod(app_path, 0755)
+
+    print "New version installed as %s" % app_path
+    print "(previous version backed up to %s)" % (backup_path)
+    return
 
 
 FILES['cpp_main.cpp']='''LyoKICogJShwcm9qZWN0X25hbWUpcwogKiBDb3B5cmlnaHQgqSAlKGF1dGhvcl9uYW1lX2Z1bGwpcyAlKGRhdGVfeWVhcilzICUoYXV0aG9yX2VtYWlsKXMKICoKICogJShwcm9qZWN0X25hbWUpcyBpcyBmcmVlIHNvZnR3YXJlOiB5b3UgY2FuIHJlZGlzdHJpYnV0ZSBpdCBhbmQvb3IgbW9kaWZ5IGl0CiAqIHVuZGVyIHRoZSB0ZXJtcyBvZiB0aGUgR05VIEdlbmVyYWwgUHVibGljIExpY2Vuc2UgYXMgcHVibGlzaGVkIGJ5IHRoZQogKiBGcmVlIFNvZnR3YXJlIEZvdW5kYXRpb24sIGVpdGhlciB2ZXJzaW9uIDMgb2YgdGhlIExpY2Vuc2UsIG9yCiAqIChhdCB5b3VyIG9wdGlvbikgYW55IGxhdGVyIHZlcnNpb24uCiAqCiAqICUocHJvamVjdF9uYW1lKXMgaXMgZGlzdHJpYnV0ZWQgaW4gdGhlIGhvcGUgdGhhdCBpdCB3aWxsIGJlIHVzZWZ1bCwgYnV0CiAqIFdJVEhPVVQgQU5ZIFdBUlJBTlRZOyB3aXRob3V0IGV2ZW4gdGhlIGltcGxpZWQgd2FycmFudHkgb2YKICogTUVSQ0hBTlRBQklMSVRZIG9yIEZJVE5FU1MgRk9SIEEgUEFSVElDVUxBUiBQVVJQT1NFLgogKiBTZWUgdGhlIEdOVSBHZW5lcmFsIFB1YmxpYyBMaWNlbnNlIGZvciBtb3JlIGRldGFpbHMuCiAqCiAqIFlvdSBzaG91bGQgaGF2ZSByZWNlaXZlZCBhIGNvcHkgb2YgdGhlIEdOVSBHZW5lcmFsIFB1YmxpYyBMaWNlbnNlIGFsb25nCiAqIHdpdGggdGhpcyBwcm9ncmFtLiAgSWYgbm90LCBzZWUgPGh0dHA6Ly93d3cuZ251Lm9yZy9saWNlbnNlcy8+LgogKi8KCiNpbmNsdWRlIDxzdGRpby5oPgojaW5jbHVkZSA8c3RkbGliLmg+CiNpbmNsdWRlIDxzdHJpbmcuaD4KI2luY2x1ZGUgPHVuaXN0ZC5oPgojaW5jbHVkZSA8Z2V0b3B0Lmg+CiNpbmNsdWRlIDxzaWduYWwuaD4KI2luY2x1ZGUgPGNjdHlwZT4KCiNkZWZpbmUgQVVUSE9SICIlKGF1dGhvcl9uYW1lX2Z1bGwpcyAlKGF1dGhvcl9lbWFpbClzIgojZGVmaW5lIFZFUlNJT04gIjAuMSIKCmNvbnN0IGNoYXIqIGFwcF9uYW1lOwoKaW5saW5lIHZvaWQgUHJpbnRVc2FnZSgpIHsKICAgIHByaW50ZigKIlVzYWdlOiAlJXMgW29wdGlvbnNdIGZvcmNlZF9hcmcgb3B0aW9uYWxfYXJnXG4iCiIgICAgJShwcm9qZWN0X2Rlc2NyaXB0aW9uKXNcbiIKIk9wdGlvbnMgYW5kIGFyZ3VtZW50czpcbiIKIiAgZm9yY2VkX2FyZyAgICAgICAgICAgICAgICAgICAgICBBIGZvcmNlZCBhcmd1bWVudFxuIgoiICBvcHRpb25hbF9hcmcgICAgICAgICAgICAgICAgICAgIEFuIG9wdGlvbiBhcmd1bWVudFxuIgoiICAtcyAtLXN3aXRjaCA8dmFsdWU+ICAgICAgICAgICAgIEEgc3dpdGNoIHdpdGggYSB2YWx1ZVxuIgoiICAtaCAtLWhlbHAgICAgICAgICAgICAgICAgICAgICAgIFByaW50cyB0aGlzIG1lc3NhZ2UuXG4iCiIgIC12IC0tdmVyYm9zZSAgICAgICAgICAgICAgICAgICAgV3JpdGVzIGFsbCBtZXNzYWdlcyB0byBjb25zb2xlLlxuXG4iCiIgICAgdiUlc1xuIiwgYXBwX25hbWUsIFZFUlNJT04pOwp9Cgp2b2lkIFNpZ0hhbmRsZXIoaW50IHNpZykgewogICAgcHJpbnRmKCJcbkdvb2RieWUuXG4iKTsKICAgIGV4aXQoMCk7Cn0KCmludCBtYWluKGludCBhcmdjLCBjaGFyKiBhcmd2W10pIHsKICAgIHNpZ25hbChTSUdJTlQsICZTaWdIYW5kbGVyKTsKCiAgICBhcHBfbmFtZSA9IGJhc2VuYW1lKGFyZ3ZbMF0pOwoKICAgIGJvb2wgdmVyYm9zZSA9IGZhbHNlOwogICAgY2hhciAqc3dpdGNoX3ZhbHVlID0gTlVMTDsKCiAgICBpbnQgYzsKICAgIGludCBvcHRpb25faW5kZXggPSAwOwoKICAgIHN0YXRpYyBzdHJ1Y3Qgb3B0aW9uIGxvbmdfb3B0aW9uc1tdID0gewogICAgICAgIC8ve2NvbnN0IGNoYXIqIG5hbWUsIGludCBoYXNfYXJnLCBpbnQqIGZsYWcsIGludCB2YWwgfSwKICAgICAgICB7ImhlbHAiLCAwLCAwLCAnaCd9LAogICAgICAgIHsidmVyYm9zZSIsIDAsIDAsICd2J30sCiAgICAgICAgeyJzd2l0Y2giLCAxLCAwLCAncyd9LAogICAgICAgIHswLCAwLCAwLCAwfQogICAgfTsKCiAgICBkbyB7CiAgICAgICAgYyA9IGdldG9wdF9sb25nKGFyZ2MsIGFyZ3YsICJodnM6IiwgbG9uZ19vcHRpb25zLCAmb3B0aW9uX2luZGV4KTsKICAgICAgICBzd2l0Y2ggKGMpIHsKICAgICAgICBjYXNlICd2JzoKICAgICAgICAgICAgdmVyYm9zZSA9IHRydWU7CiAgICAgICAgICAgIGJyZWFrOwogICAgICAgIGNhc2UgJ2gnOgogICAgICAgICAgICBQcmludFVzYWdlKCk7CiAgICAgICAgICAgIGV4aXQoMCk7CiAgICAgICAgICAgIGJyZWFrOwogICAgICAgIGNhc2UgJ3MnOgogICAgICAgICAgICAvLyBWYWxpZGF0ZSB0aGUgc3dpdGNoIHZhbHVlIGhlcmUKICAgICAgICAgICAgc3dpdGNoX3ZhbHVlID0gbmV3IGNoYXJbc3RybGVuKG9wdGFyZykrMV07CiAgICAgICAgICAgIHN0cmNweShzd2l0Y2hfdmFsdWUsIG9wdGFyZyk7CiAgICAgICAgICAgIGJyZWFrOwogICAgICAgIGNhc2UgJzonOgogICAgICAgICAgICBpZiAob3B0b3B0KSB7CiAgICAgICAgICAgICAgICBwcmludGYoIk1pc3NpbmcgYW4gYXJndW1lbnQgZm9yIGZsYWcgJy0lJWMnXG4iLCBvcHRvcHQpOwogICAgICAgICAgICB9IGVsc2UgewogICAgICAgICAgICAgICAgcHJpbnRmKCJNaXNzaW5nIGFuIGFyZ3VtZW50IGZvciBmbGFnICBcIi0tJSVzXCJcbiIsCiAgICAgICAgICAgICAgICAgICAgICAgYXJndltvcHRpbmQtMV0pOwogICAgICAgICAgICB9CiAgICAgICAgICAgIFByaW50VXNhZ2UoKTsKICAgICAgICAgICAgZXhpdCgyKTsKICAgICAgICAgICAgYnJlYWs7CiAgICAgICAgY2FzZSAnPyc6CiAgICAgICAgICAgIGlmIChvcHRvcHQpIHsKICAgICAgICAgICAgICAgIHByaW50ZigiVW5yZWNvZ25pemVkIGZsYWcgJy0lJWMnXG4iLCBvcHRvcHQpOwogICAgICAgICAgICB9IGVsc2UgewogICAgICAgICAgICAgICAgcHJpbnRmKCJVbnJlY29nbml6ZWQgZmxhZyBcIi0tJSVzXCJcbiIsIGFyZ3Zbb3B0aW5kLTFdKTsKICAgICAgICAgICAgfQogICAgICAgICAgICBQcmludFVzYWdlKCk7CiAgICAgICAgICAgIGV4aXQoMik7CiAgICAgICAgICAgIGJyZWFrOwogICAgICAgIGNhc2UgLTE6CiAgICAgICAgICAgIGJyZWFrOwogICAgICAgIGRlZmF1bHQ6CiAgICAgICAgICAgIHByaW50ZigiLS0lJWQgbm90IGNvbXBsZXRlZFxuIiwgYyk7CiAgICAgICAgICAgIGJyZWFrOwogICAgICAgIH0KICAgIH0gd2hpbGUgKGMgIT0gLTEpOwoKICAgIGNoYXIgKmZvcmNlZF9hcmcgPSBOVUxMOwogICAgY2hhciAqb3B0aW9uYWxfYXJnID0gTlVMTDsKCiAgICBpbnQgbm9ub3B0YyA9IGFyZ2MgLSBvcHRpbmQ7CiAgICBpZiAobm9ub3B0YyA+PSAxICYmIG5vbm9wdGMgPD0gMikgewogICAgICAgIC8vIFZhbGlkYXRlIHRoZSBmb3JjZWQgYXJndW1lbnQgaGVyZQogICAgICAgIGZvcmNlZF9hcmcgPSBuZXcgY2hhcltzdHJsZW4oYXJndltvcHRpbmRdKSsxXTsKICAgICAgICBzdHJjcHkoZm9yY2VkX2FyZywgYXJndltvcHRpbmRdKTsKCiAgICAgICAgaWYgKCsrb3B0aW5kIDwgYXJnYykgewogICAgICAgICAgICAvLyBWYWxpZGF0ZSB0aGUgb3B0aW9uYWwgYXJndW1lbnQgaGVyZQogICAgICAgICAgICBvcHRpb25hbF9hcmcgPSBuZXcgY2hhcltzdHJsZW4oYXJndltvcHRpbmRdKSsxXTsKICAgICAgICAgICAgc3RyY3B5KG9wdGlvbmFsX2FyZywgYXJndltvcHRpbmRdKTsKICAgICAgICB9CiAgICB9IGVsc2UgewogICAgICAgIHByaW50ZigiSW5jb3JyZWN0IG51bWJlciBvZiBhcmd1bWVudHMuXG4iKTsKICAgICAgICBQcmludFVzYWdlKCk7CiAgICAgICAgZXhpdCgyKTsKICAgIH0KCiAgICAvL1RPRE86IFJ1biBhY3R1YWwgY29kZSBoZXJlCiAgICBwcmludGYoIkhlbGxvIFdvcmxkIVxuIik7CgogICAgaWYgKHZlcmJvc2UpIHsKICAgICAgICBwcmludGYoIkVuZCBvZiBjb2RlLlxuIik7CiAgICB9CgogICAgZXhpdCgwKTsKfQo='''
